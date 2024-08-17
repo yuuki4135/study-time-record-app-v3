11b88fb907dbb8bbfe2299ec3a593121dc22e344
sugimoto.yuki <yuuki4135@gmail.com>
2024-08-17 23:12:50 +0900

    [fix]test修正

--- src/tests/App.spec.tsx
+++ src/tests/App.spec.tsx
@@ -1,5 +1,6 @@
 import App from "../App";
 import { render, screen } from "@testing-library/react";
+import '@testing-library/jest-dom';
 
 describe("title", () => {
   it("should render title", () => {
